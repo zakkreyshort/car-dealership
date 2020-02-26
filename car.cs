f07b98cc0d3456c675d0c1f44ec4336d79dbbfc3
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-02-26 07:46:37 -0800

    update code

--- car.cs
+++ car.cs
@@ -1,56 +1,38 @@
 using System;
-using System.Collections.Generic;
 
-public class Car
+namespace Dealership.Models
 {
-  public string MakeModel;
-  public int Price;
-  public int Miles;
-   public Car(string makeModel, int price, int miles)
+  public class Car
   {
-    MakeModel = makeModel;
-    Price = price;
-    Miles = miles;
-  }
-   public bool WorthBuying(int maxPrice, int maxMiles)
-   {
-    return ((Price < maxPrice && Miles < maxMiles));
-  }  
-}
+    private string _makeModel;
+    private int _price;
+    private int _miles;
 
-public class Program
-{
-  public static void Main()
-  {
-    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
-    Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-    Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
-    Car amc = new Car("1976 AMC Pacer", 400, 198000);
-
-    List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
-    Console.WriteLine("Enter maximum price: ");
-    string stringMaxPrice = Console.ReadLine();
-    int maxPrice = int.Parse(stringMaxPrice);
-    Console.WriteLine("Enter max mileage: ");
-    string stringMaxMiles = Console.ReadLine();
-    int maxMiles = int.Parse(stringMaxMiles);
-    List<Car> CarsMatchingSearch = new List<Car>(0);
-    foreach (Car automobile in Cars)
-{
-  if (automobile.WorthBuying(maxPrice, maxMiles))
-  {
-    CarsMatchingSearch.Add(automobile);
-  }
-  if (maxPrice < automobile.Price)
-  {
-    Console.WriteLine("No results to display.");
-  }
-}
-    foreach(Car automobile in CarsMatchingSearch)
+    public Car(string makeModel, int price, int miles)
     {
-      Console.WriteLine(automobile.MakeModel);
-      Console.WriteLine("price: " + automobile.Price);
-      Console.WriteLine("miles: " + automobile.Miles);
+      _makeModel = makeModel;
+      _price = price;
+      _miles = miles;
+    }
+
+    public string GetMakeModel()
+    {
+      return _makeModel;
+    }
+
+        public int GetPrice()
+    {
+      return _price;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (_price < maxPrice);
     }
   }
 }
